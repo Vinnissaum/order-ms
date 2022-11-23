9a2f67985abf7e8ee4b9353e17610e2802b849f4
Vinicius Braz <vinicius_lsb@live.com>
2022-11-23 16:56:41 -0300

    feat: implements listener for payment successful

---
+++ src/main/java/br/com/vinissaum/orders/amqp/PaymentListener.java
@@ -0,0 +1,21 @@
+package br.com.vinissaum.orders.amqp;
+
+import br.com.vinissaum.orders.dto.PaymentDTO;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PaymentListener {
+
+    @RabbitListener(queues = "payment.successful")
+    public void getMessage(PaymentDTO dto) {
+        String message = """
+                Payment id: %s
+                Value: %s
+                Order id: %s
+                """.formatted(dto.getId(), dto.getValue(), dto.getOrderId());
+
+        System.out.println(message);
+    }
+
+}
